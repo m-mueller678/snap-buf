4148ae9b66eee56fe1c0ae59686d629c7e5d7387
m-mueller678 <marcus.mueller.678@gmail.com>
2025-02-10 18:29:50 +0100

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,21 @@
+use smallvec::SmallVec;
+use std::cmp::Ordering;
+use std::mem;
 use std::num::NonZero;
+use std::ops::{Range, RangeInclusive};
 use std::sync::atomic::AtomicUsize;
 use std::sync::Arc;
 
 pub struct CowVec {
     size: usize,
-    height: usize,
+    root_height: usize,
     root: NodePointer,
 }
 
 const LEAF_SIZE: usize = 4000;
 const INNER_SIZE: usize = 500;
 
+#[derive(Clone)]
 enum CowVecNode {
     Inner([NodePointer; INNER_SIZE]),
     Leaf([u8; LEAF_SIZE]),
@@ -20,21 +25,26 @@ enum CowVecNode {
 struct NodePointer(Option<Arc<CowVecNode>>);
 
 impl NodePointer {
+    fn children(&self) -> Option<&[NodePointer; INNER_SIZE]> {
+        match &**(self.0.as_ref()?) {
+            CowVecNode::Inner(x) => Some(x),
+            CowVecNode::Leaf(_) => None,
+        }
+    }
+
     fn get_mut(&mut self, height: usize) -> &mut CowVecNode {
         let arc = self.0.get_or_insert_with(|| {
             Arc::new({
                 if height == 0 {
                     CowVecNode::Leaf([0; LEAF_SIZE])
                 } else {
-                    CowVecNode::Inner(
-                        NonZero::new(height as u8).unwrap(),
-                        array_init::array_init(|_| NodePointer(None)),
-                    )
+                    CowVecNode::Inner(array_init::array_init(|_| NodePointer(None)))
                 }
             })
         });
         Arc::make_mut(arc)
     }
+
     fn set_range(&mut self, height: usize, start: usize, values: &[u8]) {
         match self.get_mut(height) {
             CowVecNode::Inner(children) => {
@@ -44,14 +54,51 @@ impl NodePointer {
                 for child in first_child..=last_child {
                     let child_offset = first_child * child_size;
                     if child_offset <= start {
-                        children[child].set_range(start - child_offset, height - 1, values)
+                        children[child].set_range(height - 1, start - child_offset, values)
                     } else {
-                        let values = values[child_offset - start..];
-                        children[child].set_range(start - child_offset, height - 1, values)
+                        let values = &values[child_offset - start..];
+                        children[child].set_range(height - 1, start.saturating_sub(0), values)
                     }
                 }
             }
             CowVecNode::Leaf(_) => {}
+        }
+    }
+
+    fn clear_range(&mut self, height: usize, range: Range<usize>) {
+        let start = range.start;
+        let end = range.end;
+        let self_size = tree_size(height - 1);
+        if start == 0 && end >= self_size || self.0.is_none() {
+            self.0 = None;
+            return;
+        }
+        match self.get_mut(height) {
+            CowVecNode::Inner(children) => {
+                let child_size = tree_size(height - 1);
+                let first_child = start / child_size;
+                let last_child = (end - 1) / child_size;
+                for child in first_child..=last_child {
+                    let child_offset = first_child * child_size;
+                    children[child].clear_range(
+                        height - 1,
+                        start.saturating_sub(child_offset)..end - child_offset,
+                    );
+                }
+                if children.first().unwrap().0.is_none()
+                    && children.last().unwrap().0.is_none()
+                    && children.iter().all(|x| x.0.is_none())
+                {
+                    self.0 = None;
+                }
+            }
+            CowVecNode::Leaf(bytes) => {
+                let write_end = range.end.min(bytes.len());
+                bytes[start..write_end].fill(0);
+                if bytes[0] == 0 && *bytes.last().unwrap() == 0 && bytes.iter().all(|x| *x == 0) {
+                    self.0 = None;
+                }
+            }
         }
     }
 
@@ -64,7 +111,7 @@ const fn const_tree_size(height: usize) -> usize {
     if height == 0 {
         LEAF_SIZE
     } else {
-        INNER_SIZE * tree_size(height - 1)
+        INNER_SIZE * const_tree_size(height - 1)
     }
 }
 
@@ -75,8 +122,105 @@ fn tree_size(height: usize) -> usize {
 impl CowVec {
     pub fn new() -> Self {
         CowVec {
+            root_height: 0,
             size: 0,
             root: NodePointer(None),
+        }
+    }
+
+    pub fn resize(&mut self, new_size: usize) {
+        match new_size.cmp(&self.size) {
+            Ordering::Less => {
+                self.root
+                    .clear_range(self.root_height, new_size..tree_size(self.root_height));
+                self.size = new_size;
+            }
+            Ordering::Equal => {}
+            Ordering::Greater => {
+                while tree_size(self.root_height) < new_size {
+                    if self.root.0.is_some() {
+                        let mut new_root =
+                            Arc::new(CowVecNode::Inner(array_init::array_init(|x| {
+                                if x == 0 {
+                                    self.root.clone()
+                                } else {
+                                    NodePointer(None)
+                                }
+                            })));
+                        self.root = NodePointer(Some(new_root.clone()));
+                    }
+                    self.root_height += 1;
+                }
+                self.size = new_size;
+            }
+        }
+    }
+
+    /// Write data at specified offset, growing if necessary.
+    pub fn write(&mut self, offset: usize, data: &[u8]) {
+        if offset < self.size {
+            self.resize(offset + data.len());
+        }
+        self.root.set_range(self.root_height, offset, data);
+    }
+
+    pub fn len(&self) -> usize {
+        self.size
+    }
+
+    /// Clears data in range, possibly freeing memory.
+    ///
+    /// # Panics
+    /// Panics if range end is `range.end` > `self.len()`.
+    pub fn clear_range(&mut self, offset: usize, range: Range<usize>) {
+        assert!(range.end <= self.size);
+        self.root.clear_range(self.root_height, range);
+    }
+
+    /// Clears all data and sets length to 0.
+    pub fn clear(&mut self) {
+        *self = Self::new();
+    }
+
+    fn iter_nodes_pre_order(&self) -> impl Iterator<Item = (&NodePointer, usize)> {
+        struct IterStack<'a> {
+            stack_end_height: usize,
+            stack: SmallVec<[&'a [NodePointer]; 5]>,
+        }
+
+        fn split_first_in_place<'x, 's, T>(x: &'x mut &'s [T]) -> &'s T {
+            let (first, rest) = mem::replace(x, &[]).split_first().unwrap();
+            *x = rest;
+            first
+        }
+
+        impl<'a> Iterator for IterStack<'a> {
+            type Item = (&'a NodePointer, usize);
+
+            fn next(&mut self) -> Option<Self::Item> {
+                let visit_now = loop {
+                    let last_level = self.stack.last_mut()?;
+                    if last_level.is_empty() {
+                        self.stack.pop();
+                        self.stack_end_height -= 1;
+                    } else {
+                        break split_first_in_place(last_level);
+                    }
+                };
+                let ret = (visit_now, self.stack_end_height);
+                if let Some(children) = visit_now.children() {
+                    self.stack.push(children);
+                    self.stack_end_height += 1;
+                }
+                Some(ret)
+            }
+        }
+
+        let mut stack = SmallVec::new();
+        stack.push(std::slice::from_ref(&self.root));
+        IterStack {
+            stack_end_height: self.root_height,
+            stack,
         }
     }
 }
